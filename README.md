4621874694a14a6baeec3f7be877e1cfc1889dff
oya riku <>
2020-10-13 14:00:48 +0900

    リードミー編集

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## demo 
 
 https://image-uploader-839f1.web.app/
+
+## Stack
+
+- React
+- Firebase Cloud Storage
--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <ImageArea />
       </div>
+      <
     </ToastProvider>
   );
 }
