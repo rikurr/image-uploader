4fa5b46f803a534167a9f441eeda6cd6c83b2f4e
oya riku <>
2020-10-13 20:03:02 +0900

    環境変数の追加

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBGQ65cvYHsq6-7azQYP3yv9nVGhvx5MVU",
-  authDomain: "image-uploader-839f1.firebaseapp.com",
-  databaseURL: "https://image-uploader-839f1.firebaseio.com",
-  projectId: "image-uploader-839f1",
-  storageBucket: "image-uploader-839f1.appspot.com",
-  messagingSenderId: "706054871812",
-  appId: "1:706054871812:web:bf8b6d3bc2b35cac7e8e51",
-  measurementId: "G-2R7MEM4DCL",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MESUREM_ID,
 };
